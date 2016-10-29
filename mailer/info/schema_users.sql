91162e18c42dee1e33d3fcff785d964046bc2825
Denick Murray <denick@denick.org>
2016-10-29 19:08:00 +0200

    Added database schema.

---
+++ mailer/info/schema_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `config_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(40) NOT NULL DEFAULT 'denick@denick.org',
+  `birthday` date NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `i` (`user_id`,`name`,`email`,`birthday`,`config_id`),
+  KEY `c` (`config_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
