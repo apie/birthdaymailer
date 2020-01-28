65169aafb00c23a6e7dffefa7dd4202d18c6e0ee
Denick <1049485+apie@users.noreply.github.com>
2020-01-28 13:39:04 +0100

    Update schema files

--- info/schema.sql
+++ info/schema.sql
@@ -9,4 +9,12 @@ CREATE TABLE "config" (
 	`age_line`	varchar(80) NOT NULL,
 	`noage_line`	varchar(80) NOT NULL,
 	`picture_file`	varchar(80) NOT NULL
+);
+CREATE TABLE `users` (
+	`user_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`config_id`	integer NOT NULL,
+	`name`	varchar(20) NOT NULL,
+	`email`	varchar(40) NOT NULL,
+	`birthday`	date NOT NULL,
+	FOREIGN KEY (config_id) REFERENCES config(config_id)
 )
--- info/schema_users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users` (
-	`user_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`config_id`	integer NOT NULL,
-	`name`	varchar(20) NOT NULL,
-	`email`	varchar(40) NOT NULL,
-	`birthday`	date NOT NULL,
-	FOREIGN KEY (config_id) REFERENCES config(config_id)
-)
